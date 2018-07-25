55285f255ec58760c6285f731887795c8734043a
nimoh litora <maggyratoli@gmail.com>
2018-07-25 14:54:00 +0300

    add commit

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,19 @@ function readURL(input) {  if (input.files && input.files[0]) {
  }$("#imgInp").change(function() {
   readURL(this);
  });
+ function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+    }function validate() {
+    var $result = $("#result");
+    var email = $("#email").val();
+    $result.text(""); if (validateEmail(email)) {
+      $result.text(email + " is valid :)");
+      $result.css("color", "green");
+    } else {
+      $result.text(email + " is not valid :(");
+      $result.css("color", "red");
+    }
+    return false;
+    }$("#validate").bind("click", validate);
  
